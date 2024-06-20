b240fab24b4aee5b89d688d80bab832b8bb14679
AntonioMoraisMNS <AntonioMoraisMNS>
2024-06-20 14:14:55 +0000

    running notebooks in parallel

--- notebooks/02_train_models/01_train_ray.py
+++ notebooks/02_train_models/01_train_ray.py
@@ -21,15 +21,123 @@ logger = get_logger("customer-headroom")
 
 # COMMAND ----------
 
+from dtaml.databricks.runtime import Notebook, run_notebooks
 
+# COMMAND ----------
+
+seg_list = [
+        {"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_00", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_00", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_00", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_00", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_00", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_05", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_00", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_00", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_05", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_01", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_05", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_05", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_01", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_01", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_05", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_03", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_01", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_01", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_03", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_03", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_05", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_01", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_03", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_02", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_02", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_02", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_03", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_02", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_02", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_02", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_08", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_01", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_05", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_03", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_04", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_08", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_04", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_03", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_02", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_08", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_08", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_10", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_07", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_07", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_06", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_08", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_08", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_07", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_09", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_04", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_10", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_07", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_10", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_04", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_10", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_04", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_07", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_10", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_04", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_07", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_11", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_07", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_08", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_04", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_10", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_10", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_06", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_06", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_06", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_06", "segmentation": 3},
+    ]
+
+# COMMAND ----------
+
+
+notebooks = [
+  Notebook(
+    name=f"headroomX_train_campaign_{seg['campaign']}_exp_{seg['experian_hh_composition']}_seg_{seg['segmentation']}",
+    path="./02_train_single",
+    args={
+      "seg": str(seg),
+    }
+  )
+  for seg in seg_list
+]
+
+# COMMAND ----------
+
+notebooks[0].args
+
+# COMMAND ----------
+
+run_notebooks(
+  notebooks,
+  parallel=10,
+  progress=30
+)
 
 # COMMAND ----------
 
 seg = eval(dbutils.widgets.get("seg"))
 
 debug = True
-if debug:
-    seg = {"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 1}
+# if debug:
+#     seg = {"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 1}
 
 
 
@@ -164,11 +272,6 @@ def run_fit_rec(seg, config):
         description="Headroom: Registered Parameters Object",
     )
 
-
-# COMMAND ----------
-
-# MAGIC %load_ext autoreload
-# MAGIC %autoreload 2
 
 # COMMAND ----------
 
