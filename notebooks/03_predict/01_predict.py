77fd7f8fc769798a6efdcfac5456ed5e8785a95a
AntonioMoraisMNS <AntonioMoraisMNS>
2024-06-21 10:13:59 +0000

    setting task values

--- notebooks/03_predict/01_predict.py
+++ notebooks/03_predict/01_predict.py
@@ -22,14 +22,86 @@ logger = get_logger("customer-headroom")
 # COMMAND ----------
 
 seg_list = eval(dbutils.widgets.get("seg_list"))
-
-
-debug = True
-if debug:
-    seg_list = [{"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 1}]
-
-
-
+# TODO: remove this, this is just for testing purposes
+seg_list = [
+        {"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_00", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_00", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_00", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_00", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_00", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_05", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_00", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_00", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_05", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_01", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_05", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_05", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_01", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_01", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_05", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_03", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_01", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_01", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_03", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_03", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_05", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_01", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_03", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_U", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_02", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_02", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_02", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_03", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_02", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_02", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_02", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_08", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_01", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_05", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_03", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_04", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_08", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_04", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_03", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_02", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_08", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_08", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_10", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_07", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_07", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_06", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_08", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_08", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_07", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_09", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_04", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_10", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_07", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_10", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_04", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_10", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_04", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_07", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_10", "segmentation": 5},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_04", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_07", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_11", "segmentation": 0},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_07", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_08", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_04", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_10", "segmentation": 6},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_10", "segmentation": 3},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_06", "segmentation": 4},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_06", "segmentation": 1},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_06", "segmentation": 2},
+        {"campaign": 20231212, "experian_hh_composition": "Cat_06", "segmentation": 3},
+    ]
 if seg_list == []:
     dbutils.notebook.exit(True)
 else:
@@ -80,11 +152,6 @@ campaign: {campaign}
 last_registration_date: {last_registration_date}
 """
 )
-
-# COMMAND ----------
-
-# MAGIC %load_ext autoreload
-# MAGIC %autoreload 2
 
 # COMMAND ----------
 
