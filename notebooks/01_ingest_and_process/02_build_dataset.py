1429ea05c1f508f77a2f9bcf2d5c0f7550149067
AntonioMoraisMNS <antonio.morais@mnscorp.net>
2024-06-25 16:31:59 +0100

    build_dataset widget

--- notebooks/01_ingest_and_process/02_build_dataset.py
+++ notebooks/01_ingest_and_process/02_build_dataset.py
@@ -73,183 +73,156 @@ logger.info(f"Segmentations: {seg_list}")
 
 # COMMAND ----------
 
-# Step 4: Build Dataset
-
-config_use = config["use_segments"]
-
-logger.info("Begin building dataset")
-config_bd = config["build_dataset"]
-
-# load factory tables
-articles_df = spark.table("analytics_trans_prod.lu_article")
-trx_line_df = spark.table("analytics_trans_prod.all_transaction_line")
-sparks_account_df = spark.table("analytics_trans_prod.sparks_account")
-segtco_history_df = spark.table("customer_azbase_prod.segtco_history")
-
+build_dataset = widgets.get("build_dataset", "True")
 
 # COMMAND ----------
 
+if build_dataset == "True":
+    # Step 4: Build Dataset
 
-# Load Segmentation Dataset
-# TODO: Replace with customer cluster work to reduce data sizes to appropiate groups.
-# TODO: Possibl build data for just 1 segment at a time?
-partitionByList = config_use.segmentations_tbl.partitionByList
+    config_use = config["use_segments"]
 
-# build training data
-trx_manager = TransactionsManager(
-    etl_date=get_date(config.dates.etl_date),
-    lookback_days=config.dates.lookback_days,
-    l1_ids=config_bd["l1_ids"],
-    lx=config_bd["lx"],
-    lx_ids=config_bd["lx_ids"],  # getting all the products in this l2 id
-    user_key=config_bd["user_id"],
-    window_days=config_bd["window_days"],
-    time_window_length=config_bd["time_window_days"],
-    exclude_items=literal_eval(config["exclude_items"]),
-    aggregation_level=config_bd["aggregation_level"],
-)
-
-# COMMAND ----------
-
-
-all_data = trx_manager.get(trx_line_df, articles_df, cust_seg=segmentations_tbl)
-
-
-# COMMAND ----------
-
-
-etl_data_tbl_name = persist_utils.create_beam_table(
-    table_prefix=config_bd.etl_data_tbl.prefix,
-    lab_database=config.lab_database,
-    factory_database=config.factory_database,
-    sensitivity=config.sensitivity,
-    schema=all_data,
-    partition_by=config_bd.etl_data_tbl.partitionByList,
-    overwrite_table=False,
-    assert_equality=False,
-    add_load_timestamp=True,
-)
-logger.info(f"""etl_data_tbl_name: {etl_data_tbl_name}""")
-
-persist_utils.insert_df_into_table(
-    target_tbl_name=etl_data_tbl_name,
-    insert_df=all_data,
-    add_columns=True,
-    insert_append=True,
-    delete_where=f"campaign={campaign}",
-)
-
-# COMMAND ----------
-
-# Step 4: Build Dataset
-
-config_use = config["use_segments"]
-
-logger.info("Begin building dataset")
-config_bd = config["build_dataset"]
-
-# # load factory tables
-# articles_df = spark.table("analytics_trans_prod.lu_article")
-# trx_line_df = spark.table("analytics_trans_prod.all_transaction_line")
-# sparks_account_df = spark.table("analytics_trans_prod.sparks_account")
-# segtco_history_df = spark.table("customer_azbase_prod.segtco_history")
-
-# Load Segmentation Dataset
-# TODO: Replace with customer cluster work to reduce data sizes to appropiate groups.
-# TODO: Possibl build data for just 1 segment at a time?
-partitionByList = config_use.segmentations_tbl.partitionByList
-
-segmentations_tbl_name = factory_table(
-    table_prefix=config_bd.segmentations_tbl.prefix, sensitivity=config.sensitivity
-)
-
-logger.info(f"""segmentations_tbl_name: {segmentations_tbl_name}""")
-
-segmentations_tbl = persist_utils.read_table(
-    table_name=segmentations_tbl_name, where=f"campaign= {campaign}"
-).select([config_bd["user_id"]] + partitionByList)
-
-
-# COMMAND ----------
-
-
-# build training data
-trx_manager = TransactionsManager(
-    etl_date=get_date(config.dates.etl_date),
-    lookback_days=config.dates.lookback_days,
-    l1_ids=config_bd["l1_ids"],
-    lx=config_bd["lx"],
-    lx_ids=config_bd["lx_ids"],  # getting all the products in this l2 id
-    user_key=config_bd["user_id"],
-    window_days=config_bd["window_days"],
-    time_window_length=config_bd["time_window_days"],
-    exclude_items=literal_eval(config["exclude_items"]),
-    aggregation_level=config_bd["aggregation_level"],
-)
-
-# COMMAND ----------
-
-
-all_data = trx_manager.get(trx_line_df, articles_df, cust_seg=segmentations_tbl)
-
-
-# COMMAND ----------
-
-
-etl_data_tbl_name = persist_utils.create_beam_table(
-    table_prefix=config_bd.etl_data_tbl.prefix,
-    lab_database=config.lab_database,
-    factory_database=config.factory_database,
-    sensitivity=config.sensitivity,
-    schema=all_data,
-    partition_by=config_bd.etl_data_tbl.partitionByList,
-    overwrite_table=False,
-    assert_equality=False,
-    add_load_timestamp=True,
-)
-logger.info(f"""etl_data_tbl_name: {etl_data_tbl_name}""")
-
-persist_utils.insert_df_into_table(
-    target_tbl_name=etl_data_tbl_name,
-    insert_df=all_data,
-    add_columns=True,
-    insert_append=True,
-    delete_where=f"campaign={campaign}",
-)
-
-# TODO: Save cust_id to account_id Mapping as done in the customer_purchase work.
-# TODO: delete all mentions of validationmanager
-
-# COMMAND ----------
-
-# MAGIC %load_ext autoreload
-# MAGIC %autoreload 2
-
-# COMMAND ----------
-
-# Step 5: Get ordered segmentation list
-seg_cnt = []
-for seg in seg_list:
+    logger.info("Begin building dataset")
     config_bd = config["build_dataset"]
-    seg_cnt.append(
-        get_count(
-            seg,
-            config=config,
-        )
+
+    # load factory tables
+    articles_df = spark.table("analytics_trans_prod.lu_article")
+    trx_line_df = spark.table("analytics_trans_prod.all_transaction_line")
+    sparks_account_df = spark.table("analytics_trans_prod.sparks_account")
+    segtco_history_df = spark.table("customer_azbase_prod.segtco_history")
+
+    # Load Segmentation Dataset
+    # TODO: Replace with customer cluster work to reduce data sizes to appropiate groups.
+    # TODO: Possibl build data for just 1 segment at a time?
+    partitionByList = config_use.segmentations_tbl.partitionByList
+
+    # build training data
+    trx_manager = TransactionsManager(
+        etl_date=get_date(config.dates.etl_date),
+        lookback_days=config.dates.lookback_days,
+        l1_ids=config_bd["l1_ids"],
+        lx=config_bd["lx"],
+        lx_ids=config_bd["lx_ids"],  # getting all the products in this l2 id
+        user_key=config_bd["user_id"],
+        window_days=config_bd["window_days"],
+        time_window_length=config_bd["time_window_days"],
+        exclude_items=literal_eval(config["exclude_items"]),
+        aggregation_level=config_bd["aggregation_level"],
     )
 
-seg_cnt.sort(key=lambda i: i[1], reverse=True)
+    all_data = trx_manager.get(trx_line_df, articles_df, cust_seg=segmentations_tbl)
 
-seg_list = [seg[0] for seg in seg_cnt]
-logger.info(f"Ordered seg_list: {seg_list}")
+    etl_data_tbl_name = persist_utils.create_beam_table(
+        table_prefix=config_bd.etl_data_tbl.prefix,
+        lab_database=config.lab_database,
+        factory_database=config.factory_database,
+        sensitivity=config.sensitivity,
+        schema=all_data,
+        partition_by=config_bd.etl_data_tbl.partitionByList,
+        overwrite_table=False,
+        assert_equality=False,
+        add_load_timestamp=True,
+    )
+    logger.info(f"""etl_data_tbl_name: {etl_data_tbl_name}""")
+
+    persist_utils.insert_df_into_table(
+        target_tbl_name=etl_data_tbl_name,
+        insert_df=all_data,
+        add_columns=True,
+        insert_append=True,
+        delete_where=f"campaign={campaign}",
+    )
+
+    # Step 4: Build Dataset
+
+    config_use = config["use_segments"]
+
+    logger.info("Begin building dataset")
+    config_bd = config["build_dataset"]
+
+    # Load Segmentation Dataset
+    # TODO: Replace with customer cluster work to reduce data sizes to appropiate groups.
+    # TODO: Possibl build data for just 1 segment at a time?
+    partitionByList = config_use.segmentations_tbl.partitionByList
+
+    segmentations_tbl_name = factory_table(
+        table_prefix=config_bd.segmentations_tbl.prefix, sensitivity=config.sensitivity
+    )
+
+    logger.info(f"""segmentations_tbl_name: {segmentations_tbl_name}""")
+
+    segmentations_tbl = persist_utils.read_table(
+        table_name=segmentations_tbl_name, where=f"campaign= {campaign}"
+    ).select([config_bd["user_id"]] + partitionByList)
+
+    # build training data
+    trx_manager = TransactionsManager(
+        etl_date=get_date(config.dates.etl_date),
+        lookback_days=config.dates.lookback_days,
+        l1_ids=config_bd["l1_ids"],
+        lx=config_bd["lx"],
+        lx_ids=config_bd["lx_ids"],  # getting all the products in this l2 id
+        user_key=config_bd["user_id"],
+        window_days=config_bd["window_days"],
+        time_window_length=config_bd["time_window_days"],
+        exclude_items=literal_eval(config["exclude_items"]),
+        aggregation_level=config_bd["aggregation_level"],
+    )
+
+    all_data = trx_manager.get(trx_line_df, articles_df, cust_seg=segmentations_tbl)
+
+    etl_data_tbl_name = persist_utils.create_beam_table(
+        table_prefix=config_bd.etl_data_tbl.prefix,
+        lab_database=config.lab_database,
+        factory_database=config.factory_database,
+        sensitivity=config.sensitivity,
+        schema=all_data,
+        partition_by=config_bd.etl_data_tbl.partitionByList,
+        overwrite_table=False,
+        assert_equality=False,
+        add_load_timestamp=True,
+    )
+    logger.info(f"""etl_data_tbl_name: {etl_data_tbl_name}""")
+
+    persist_utils.insert_df_into_table(
+        target_tbl_name=etl_data_tbl_name,
+        insert_df=all_data,
+        add_columns=True,
+        insert_append=True,
+        delete_where=f"campaign={campaign}",
+    )
+
+    # TODO: Save cust_id to account_id Mapping as done in the customer_purchase work.
+    # TODO: delete all mentions of validationmanager
+
+    # Step 5: Get ordered segmentation list
+    seg_cnt = []
+    for seg in seg_list:
+        config_bd = config["build_dataset"]
+        seg_cnt.append(
+            get_count(
+                seg,
+                config=config,
+            )
+        )
+
+    seg_cnt.sort(key=lambda i: i[1], reverse=True)
+
+    seg_list = [seg[0] for seg in seg_cnt]
+    logger.info(f"Ordered seg_list: {seg_list}")
+
 
 # COMMAND ----------
 
-dbutils.jobs.taskValues.set(key="seg_list", value=seg_list)
+
+import json
+
+str_seg_list = json.dumps(seg_list, indent=2)
+dbutils.jobs.taskValues.set(key="seg_list", value=str_seg_list)
 
 # COMMAND ----------
 
 # seg_list has
-dbutils.notebook.exit(str({"seg_list": seg_list}))
+dbutils.notebook.exit(str({"seg_list": str_seg_list}))
 
 # COMMAND ----------
